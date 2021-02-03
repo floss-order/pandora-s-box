821cbc2262e7884d415df359d1093417ea96d1ef
floss-order <flossOrder@gmail.com>
2021-02-03 18:11:07 +0000

    Added some styling for the header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,29 @@
-import { Container, Toolbar, Typography, Button } from '@material-ui/core'
+import { Toolbar, Typography, Button } from '@material-ui/core'
+import { Link } from 'react-router-dom'
+
+import './Header.css'
 
 function Header() {
 	return (
 		<header>
-			<Toolbar>
+			<Toolbar className="toolbar">
 				<Typography component="p">Pandora's box</Typography>
-				<Button variant="contained" color="primary">Sign In</Button>
-				<Button variant="text" color="secondary">Sign Out</Button>
+
+				<nav className="nav">
+					<ul className="nav__list">
+						<li className="nav__list-item">
+							<Link to="/signin">
+								<Button variant="contained" color="primary">Sign In</Button>
+							</Link>
+						</li>
+						<li className="nav__list-item">
+							<Link to="/signup">
+								<Button variant="text" color="secondary">Sign Up</Button>
+							</Link>
+						</li>
+					</ul>
+				</nav>
+
 			</Toolbar>
 		</header>
 	)
