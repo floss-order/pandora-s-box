4dbb97a88242ab5693651a3577e8670563f940ed
floss-order <flossOrder@gmail.com>
2021-02-03 16:55:55 +0000

    Added sign up route

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import { ThemeProvider } from '@material-ui/core'
 import theme from './theme'
+import { Switch, Route } from 'react-router-dom'
+import SignUp from './pages/SignUp'
 
 function App() {
   return (
 	  <ThemeProvider theme={theme}>
-
+		  <Switch>
+			  <Route path="/signup" component={SignUp} />
+		  </Switch>
 	  </ThemeProvider>
   )
 }
