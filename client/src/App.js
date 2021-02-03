1dedf838bcac26de044c63433d7e130fefe6fc13
floss-order <flossOrder@gmail.com>
2021-02-03 17:00:02 +0000

    Added sign in route

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import { ThemeProvider } from '@material-ui/core'
 import theme from './theme'
 import { Switch, Route } from 'react-router-dom'
 import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
 
 function App() {
   return (
 	  <ThemeProvider theme={theme}>
 		  <Switch>
 			  <Route path="/signup" component={SignUp} />
+			  <Route path="/signin" component={SignIn} />
 		  </Switch>
 	  </ThemeProvider>
   )
