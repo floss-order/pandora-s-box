7f0114054f90509bd2212230ba5857cdb4747221
floss-order <flossOrder@gmail.com>
2021-02-03 16:59:14 +0000

    Added sign in page

---
+++ client/src/pages/SignIn.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container } from '@material-ui/core'
+import SignInForm from '../components/SignInForm'
+
+function SignIn() {
+	return (
+		<Container maxWidth="sm">
+			<SignInForm />
+		</Container>
+	)
+}
+
+export default SignIn
