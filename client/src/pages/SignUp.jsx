9acf34ad69e858bf60f53f551ff31cd936e6e32c
floss-order <flossOrder@gmail.com>
2021-02-03 16:55:23 +0000

    Created sign up page

---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container } from '@material-ui/core'
+import SignUpForm from '../components/SignUpForm'
+
+function SignUp() {
+	return (
+		<Container maxWidth="sm">
+			<SignUpForm />
+		</Container>
+	)
+}
+
+export default SignUp
