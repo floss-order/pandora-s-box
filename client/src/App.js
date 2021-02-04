d5b5563a59264178a941fb8ff62a01ffed3a5024
floss-order <flossOrder@gmail.com>
2021-02-04 07:29:11 +0000

    Created files route

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import { Switch, Route } from 'react-router-dom'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import Header from './components/Header'
+import Files from './pages/Files'
 
 function App() {
   return (
 	  <ThemeProvider theme={theme}>
 		<Header />
 		<Switch>
+			<Route path='/' component={Files} exact />
 			<Route path="/signup" component={SignUp} />
 			<Route path="/signin" component={SignIn} />
 		</Switch>
