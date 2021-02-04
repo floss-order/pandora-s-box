fcd0e6292ed171609774474aea5734bd7928548d
floss-order <flossOrder@gmail.com>
2021-02-04 04:31:13 +0000

    Created a form validation schema

---
+++ client/src/formValidation.js
@@ -0,0 +1,17 @@
+import * as yup from 'yup'
+
+const requiredMessage = 'This field is required'
+const emailNotValidMessage = 'Not a valid email'
+const emailDoesNotExistMessage = 'Email does not exist'
+const passwordsMatchMessage = 'Passwords must match'
+
+export const signUpSchema = yup.object().shape({
+    email: yup.string().required(requiredMessage).email(emailNotValidMessage),
+    password: yup.string().required(requiredMessage).min(6),
+    passwordConfirm: yup.string().oneOf([yup.ref('password'), null], passwordsMatchMessage)
+})
+
+export const signInSchema = yup.object().shape({
+    email: yup.string().required(requiredMessage).email(emailNotValidMessage),
+    password: yup.string().required(requiredMessage)
+})
