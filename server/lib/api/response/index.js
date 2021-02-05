0bb243d438eb1ff52e396e1731b5600520d28efa
floss-order <flossOrder@gmail.com>
2021-02-05 16:32:43 +0000

    Response handling helper functions

---
+++ server/lib/api/response/index.js
@@ -0,0 +1,50 @@
+const ApplicationError, createError = require('../error') 
+
+function formatError(error, overrides = {}) {
+  // `Error.stack`'s `enumerable` property descriptor is `false`
+  // Thus, `JSON.stringify(...)` doesn't enumerate over it.
+  const stackTrace = JSON.stringify(error, ['stack'], 4) || {}
+  const newError = JSON.parse(JSON.stringify(error))
+
+  // No need to send to client
+  newError.statusCode = undefined
+  delete newError.meta
+
+  return {
+    error: {
+      ...newError,
+      stack: stackTrace.stack
+    },
+    success: false,
+    ...overrides
+  }
+}
+
+function formatResponse(result, override = {}) {
+  return {
+    data: result,
+    success: true,
+    ...override
+  }
+}
+
+function sendResponse(res, payload, statusCode = 200, context = {}) {
+  if (payload instanceof ApplicationError) {
+    const code = payload.statusCode || 500
+    return res.status(code).json(formatError(payload))
+  }
+
+  if (payload instanceof Error) {
+    const newError = createError(payload)
+    const code = newError.statusCode || 500
+    return res.status(code).json(formatError(newError))
+  }
+
+  return res.status(statusCode).json(formatResponse(payload))
+}
+
+module.exports = {
+  formatError,
+  formatResponse,
+  sendResponse
+}
