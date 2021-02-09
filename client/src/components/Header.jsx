4f02a7438153ccd4dc0e2b23dcb91f56e9cbd8cb
floss-order <flossOrder@gmail.com>
2021-02-09 21:02:52 +0000

    Added a change of navigation buttons inside the header depending on the user's authentication status

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,13 @@
+import React, { useContext } from 'react'
 import { Toolbar, Typography, Button } from '@material-ui/core'
 import { Link } from 'react-router-dom'
+import { AuthContext } from '../context/AuthContext'
 
 import './Header.css'
 
 function Header() {
+	const { isAuthenticated } = useContext(AuthContext)
+
 	return (
 		<header>
 			<Toolbar className="toolbar">
@@ -11,16 +15,44 @@ function Header() {
 
 				<nav className="nav">
 					<ul className="nav__list">
-						<li className="nav__list-item">
-							<Link to="/signin">
-								<Button variant="contained" color="primary">Sign In</Button>
-							</Link>
-						</li>
-						<li className="nav__list-item">
-							<Link to="/signup">
-								<Button variant="text" color="secondary">Sign Up</Button>
-							</Link>
-						</li>
+						{isAuthenticated 
+						
+						? 
+						
+						(
+							<>
+								<li>
+									<Link to="/">
+										<Button variant="contained" color="primary">My files</Button>
+									</Link>
+								</li>
+								<li>
+									<Link to="/">
+										<Button variant="text" color="secondary">Sign Out</Button>
+									</Link>
+								</li>
+								
+							</>
+						)
+
+						: 
+						
+						(
+							<>
+								<li className="nav__list-item">
+									<Link to="/signin">
+										<Button variant="contained" color="primary">Sign In</Button>
+									</Link>
+								</li>
+								<li className="nav__list-item">
+									<Link to="/signup">
+										<Button variant="text" color="secondary">Sign Up</Button>
+									</Link>
+								</li>
+							</>
+						)
+						}
+
 					</ul>
 				</nav>
 
